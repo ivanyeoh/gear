29144d2766dbdd07ac96b132e3f7f0fbd5d82d03
Ivan Yeoh <ivanbone@gmail.com>
2014-09-15 02:01:51 +0800

    added page stats

--- src/gear-page.js
+++ src/gear-page.js
@@ -1,12 +1,14 @@
 angular.module('gear.page', [])
-    .factory('grPage', function () {
-        var page = {
-            title: '',
-            desc: ''
-        };
-        return page;
+    .run(function ($rootScope) {
+        $rootScope.grPage = {};
+        $rootScope.$on('$routeChangeStart', function (e, current) {
+            if (!current.$$route) return;
+            var routePage = current.$$route.page;
+            $rootScope.grPage.title = routePage.title;
+            $rootScope.grPage.desc = routePage.desc;
+        });
     })
-    .directive('grPageHeader', function () {
+    .directive('grPageHeader', function (gr, $rootScope) {
         return {
             restrict: 'E',
             replace: true,
@@ -14,10 +16,41 @@ angular.module('gear.page', [])
                 title: '@',
                 desc: '@'
             },
+            transclude: true,
             template: '<div class="page-header">' +
                 '<div class="page-title">' +
                     '<h3>{{title}}</h3><span>{{desc}}</span>' +
                 '</div>' +
-            '</div>'
+            '</div>',
+            link: function (scope, element, attrs, ctrl, transclude) {
+                $rootScope.$watch('grPage', function () {
+                    scope.title = $rootScope.grPage.title;
+                    scope.desc = $rootScope.grPage.desc;
+                }, true);
+
+                transclude(scope, function (tElements) {
+                    element.append(tElements);
+                });
+            }
+        }
+    })
+    .directive('grPageStats', function () {
+        return {
+            restrict: 'E',
+            replace: true,
+            transclude: true,
+            template: '<ul class="page-stats" ng-transclude></ul>'
+        }
+    })
+    .directive('grPageStat', function () {
+        return {
+            restrict: 'E',
+            replace: true,
+            transclude: true,
+            scope: {
+                title: '@',
+                desc: '@'
+            },
+            template: '<li><div class="summary"><span>{{title}}</span><h3>{{desc}}</h3></div></li>'
         }
     })
